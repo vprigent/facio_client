4679c74354f6c6be3c2acfa97ad855082a06b9a5
Vincent Prigent <vincent.prigent@hotmail.fr>
2018-01-20 00:32:05 +1300

    Add description to item model and form

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,25 +1,39 @@
 <template>
   <li class="item"
-    :class=item.id
+    :class="[ item.id, {'editing': (editedItem == item)}]"
     draggable
     @dragstart="dragItem($event, item)"
     @dragend="dropItem($event, item)"
     >
     <input :id="'check-item-' + item.id" v-model="done" type="checkbox">
-    <input v-if="editedItem == item" type="text" v-model="item.label" @keydown.enter="changeItem">
-    <label v-else :for="'check-item-' + item.id">{{ item.label }}</label>
 
     <submenu class="pull-right">
       <ul class="actions submenu">
         <li>
-          <a href='#' class="delete" @click="removeItem()">Delete</a>
+          <a href='' class="delete" @click.prevent="removeItem()">Delete</a>
         </li>
         <li>
-          <a href='#' class="edit" @click="editItem($event)">Edit</a>
+          <a href='' class="edit" @click.prevent="editItem($event)">Edit</a>
         </li>
       </ul>
     </submenu>
-    <div class="desc">{{ item.description }}</div>
+
+    <form v-if="editedItem == item" method="post" @submit.prevent="changeItem">
+      <input v-model="item.label" type="text" name="item_label">
+
+      <div class="form-group">
+        <label for="item_description">Description</label>
+        <input v-model="item.description" type="textarea" name="item_description">
+      </div>
+
+      <button type="submit" name="button">Submit</button>
+    </form>
+
+    <span v-else>
+      <label  :for="'check-item-' + item.id">{{ item.label }}</label>
+
+      <div class="desc">{{ item.description }}</div>
+    </span>
   </li>
 </template>
 
@@ -62,11 +76,11 @@ export default {
       this.deleteItem(this.item)
     },
     editItem: function (event) {
-      event.target.parentNode.parentNode.classList.remove('active')
+      event.currentTarget.parentNode.parentNode.classList.remove('active')
       this.editedItem = this.item
     },
     changeItem: function (event) {
-      this.updateItem({record: this.item, label: event.target.value})
+      this.updateItem({record: this.item, label: this.item.label, description: this.item.description})
       this.editedItem = null
     },
     dragItem: function (event, item) {
--- src/models/item.js
+++ src/models/item.js
@@ -6,10 +6,11 @@ export default class Item extends Record {
 
     this.id = attrs['id']
     this.label = attrs['label']
+    this.description = attrs['description']
     this.done = false
     this.list_id = attrs['list_id']
   }
 }
 
-Item.attributes = ['label', 'done', 'list_id']
+Item.attributes = ['label', 'done', 'description', 'list_id']
 Item.tableName = 'items'
